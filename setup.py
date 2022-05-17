e2bc8938ec93639f7dd114c97675674765645a94
camfairchild <cameron@fairchild.dev>
2022-05-17 12:03:44 -0400

    fix attribution

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 # https://github.com/rmcgibbo/npcuda-example/blob/master/cython/setup.py
 #
 # Copyright (c) 2014, Robert T. McGibbon and the Authors
+# Copyright (c) 2022 Cameron Fairchild
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
